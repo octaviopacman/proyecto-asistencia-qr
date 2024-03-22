c385131f87bbf7d97251ce468ecca35f110c418d
0lio21 <109021108+0lio21@users.noreply.github.com>
2024-03-21 21:20:12 -0300

    Actualizar App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import MiComponente from './components/micomponente';
-
-
-
-
+import Registro from './components/registro'
 
 
 function App() {
+  const[mostrarLogin, setmostrarLogin] =useState(false);
+  const[mostrarRegistro, setmostrarRegistro] =useState(false);
+ 
+  const login = {
+    display:mostrarLogin ? 'block' : 'none',
 
-
+  };
+  const register ={
+    display: mostrarRegistro ? 'block': 'none',
+  };
   return (
     <div className="App">
-      <MiComponente/>
+      <button onClick={() => setmostrarLogin(!mostrarLogin)}>Mostrar/Ocultar Login</button>
+      <button onClick={() => setmostrarRegistro(!mostrarRegistro)}>Mostrar/Ocultar Registro</button>
+      <div className='login' style={login}>
+        <MiComponente/>
+      </div>
+      <div className='register' style={register}>
+        <Registro/>
+      </div>
+      
     </div>
   );
 }
