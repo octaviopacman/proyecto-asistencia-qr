c7270222fed038c63262b62025553f376ba11e21
octaviopacman <70413021+octaviopacman@users.noreply.github.com>
2024-04-07 02:02:45 -0300

    Actualizar react-front

---
+++ react-front/src/App.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react';
+import Login from './components/login';
+import Registro from './components/registro'
+import './App.css'
+
+
+function App() {
+  
+  const[estaRegistrado, setestaRegistrado] =useState(false);
+ 
+  
+  let contenido;
+  let contenidoBoton;
+  if (estaRegistrado){
+    contenido = <Registro/>;
+    contenidoBoton = 'Volver al inicio de sesion';
+  } else {
+    contenido = <Login/>;
+    contenidoBoton = 'No estas registrado?';
+  }
+
+  return (
+    <div className="App">
+      {contenido}
+      <button onClick={() => setestaRegistrado(!estaRegistrado)}>{contenidoBoton}</button>
+    </div>
+  );
+}
+
+export default App;
