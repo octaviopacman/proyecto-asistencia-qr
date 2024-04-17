f9df2043ac517a00bac5844122f4d246c719dd47
octaviopacman <70413021+octaviopacman@users.noreply.github.com>
2024-04-17 13:44:34 -0300

    Port a Next.js

--- react-front/src/app/components/registronuevo.jsx
+++ react-front/src/app/components/registronuevo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import "./registro.css"
-export default function RegistroNuevo() {
+export default function Registro() {
 
   const {
     register,
