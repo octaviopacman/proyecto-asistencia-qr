8f93af754e91fab58d2db4684ef1b34ec82e2fde
0lio21 <kenyiar90@gmail.com>
2024-05-03 02:51:43 -0300

    backend

--- backend-node/database/db.js
+++ backend-node/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('bdtecnica', 'root', '4532',{
+const db = new Sequelize('bdtecnica', 'root', '1234',{
     host:'localhost',
     dialect: 'mysql',
 })
