3128eb8e9703d56ff26c20f461f0964f176536ed
octaviopacman <70413021+octaviopacman@users.noreply.github.com>
2024-10-17 16:11:25 -0300

    Update conexiones.jsx

--- react-front/src/app/assets/conexiones.jsx
+++ react-front/src/app/assets/conexiones.jsx
@@ -21,6 +21,25 @@ export class Dashboard {
             throw new Error('Error al obtener asistencias');
         }
     }
+    async obtenerMaterias(){
+        console.log(this.token);
+        try {
+            const response = await fetch('https://backend-asistencia-qr.vercel.app/api/listado/materias', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'autorizacion': `Bearer ${this.token}`,
+                },
+                credentials: 'include'
+            });
+            const data = await response.json();
+            return data;
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+            throw new Error('Error al obtener materias');
+        }
+    }
 }
 
 
