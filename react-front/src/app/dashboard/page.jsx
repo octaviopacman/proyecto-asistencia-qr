d5ffde5ee377ad2e2d001087f593497d598cf473
octaviopacman <70413021+octaviopacman@users.noreply.github.com>
2024-10-17 19:02:07 -0300

    style donutchart

--- react-front/src/app/dashboard/page.jsx
+++ react-front/src/app/dashboard/page.jsx
@@ -127,7 +127,7 @@ function Admin() {
                                 <Card.Body>
                                     <Card.Title>Asistencias</Card.Title>
                                     {data ? (
-                                        <DonutChart attendance={data.asistencias} absence={data.inasistencias} />
+                                        <DonutChart className={styles.donutchart} attendance={data.asistencias} absence={data.inasistencias} />
                                     ) : (
                                         <Card.Text>No tienes asistencias registradas</Card.Text>
                                     )}
