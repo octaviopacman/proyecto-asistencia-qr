7e08f84ec5295a864ceb4de19b066b4d75087974
octaviopacman <70413021+octaviopacman@users.noreply.github.com>
2024-10-27 02:54:22 -0300

    Update session.jsx

--- react-front/src/app/assets/session.jsx
+++ react-front/src/app/assets/session.jsx
@@ -1,19 +1,20 @@
-"use client"
+"use client";
 import { createContext, useState, useEffect, useContext } from "react";
 import Cookies from "js-cookie";
 
 const SessionContext = createContext();
 
 export const SessionProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    message: '',
-    token: '',
+  const [user, setUser] = useState(() => {
+    // Estado inicial de user, intentando recuperar de las cookies al cargar
+    const savedUser = Cookies.get("user");
+    return savedUser ? JSON.parse(savedUser) : { message: '', token: '' };
   });
 
   useEffect(() => {
-    // Al cargar la página, intenta recuperar el usuario de las cookies
+    // Al cargar la página, intenta recuperar el usuario de las cookies si no se ha hecho antes
     const savedUser = Cookies.get("user");
-    if (savedUser) {
+    if (savedUser && !user.token) {
       setUser(JSON.parse(savedUser));
     }
   }, []);
@@ -24,7 +25,7 @@ export const SessionProvider = ({ children }) => {
   };
 
   const logout = () => {
-    setUser(null);
+    setUser({ message: '', token: '' }); // Restaurar el estado inicial vacío
     Cookies.remove("user"); // Eliminar sesión de cookies
   };
 
