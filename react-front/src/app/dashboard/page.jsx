efcb43e2e1e1436ae83f3dbda54fdc8d2d162428
AlejoCavuto <165060130+AlejoCavuto@users.noreply.github.com>
2024-11-06 20:48:03 -0300

    Update page.jsx

--- react-front/src/app/dashboard/page.jsx
+++ react-front/src/app/dashboard/page.jsx
@@ -1,14 +1,5 @@
-'use client';
 import { useState, useEffect } from 'react';
-import {
-    Nav,
-    Navbar,
-    Container,
-    Card,
-    Col,
-    Row,
-    Button,
-} from 'react-bootstrap';
+import { Nav, Navbar, Container, Card, Col, Row, Button, Dropdown, Alert } from 'react-bootstrap';
 import QRCodeComponent from '../login/codigo';
 import styles from './page.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,72 +7,40 @@ import DonutChart from './donutchart';
 import { useRouter } from 'next/navigation';
 import { Dashboard } from '../assets/conexiones';
 import { useSession } from "../assets/session";
-import { Dropdown } from 'react-bootstrap';
-import AdminPage from "../admin/page"
-
+import AdminPage from "../admin/page";
 
 function Admin() {
     const [data, setData] = useState({
         asistencias: { asistencias: 0, inasistencias: 0 },
-        materias: {},
-        horarioProfesor: {},
-        horarioCurso: {},
-        cursos: {},
-        profesores: {}
+        horarioProfesor: []
     });
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const { user, login, logout } = useSession();
-
+    const { user } = useSession();
     const Router = useRouter();
-
-    console.log(user);
-    console.log(user.token);
-
-
-
 
     const dashboard = new Dashboard(user.token);
 
     useEffect(() => {
-        // Función asíncrona dentro de useEffect
         const fetchData = async () => {
             try {
-                // Obtener todos los datos del dashboard
                 const asistencias = await dashboard.contarAsistencias();
-                /* const materias = await dashboard.obtenerMaterias(); */
-                const horarioProfesor = await dashboard.mostrarHorarioProfesor();
-                //const horarioCurso = await dashboard.mostrarHorarioCurso();
+                const horarioProfesor = await dashboard.mostrarHorarioProfesor(user.profesorId); // Usa el ID del profesor actual
 
-
-
-                // Combinar todos los datos en un solo objeto
                 const resultados = {
-                    asistencias: asistencias || {}, // Valor predeterminado vacío si es undefined
-                    /* materias: materias || {}, */
-                    horarioProfesor: horarioProfesor || {},
-                    //horarioCurso: horarioCurso || {},
+                    asistencias: asistencias || {},
+                    horarioProfesor: horarioProfesor || []
                 };
 
-                // Establecer los datos combinados
                 setData(resultados);
             } catch (error) {
                 console.error("Error al obtener los datos del dashboard:", error);
-                // Manejar el error según sea necesario
+                setError("Hubo un problema al cargar los datos.");
             }
         };
 
         fetchData();
     }, []);
-
-
-    console.log(data);
-
-
-
-
-
-
 
     return (
         <div>
@@ -95,75 +54,67 @@ function Admin() {
                 </Nav>
                 <Nav>
                     <Dropdown drop='start'>
-                        <Dropdown.Toggle>
-                            Administración
-                        </Dropdown.Toggle>
+                        <Dropdown.Toggle>Administración</Dropdown.Toggle>
                         <Dropdown.Menu>
-                            <AdminPage></AdminPage>
+                            <AdminPage />
                         </Dropdown.Menu>
                     </Dropdown>
                 </Nav>
             </Navbar>
 
-            <div className={styles.tarjetas}>
-                <Container>
-                    <Row>
-                        <Col md={8} className="mb-4">
-                            <Card className={`${styles.tarjeta} ${styles['tarjeta-asistencias']}`}>
-                                <Card.Body>
-                                    <Card.Title>Asistencias</Card.Title>
-                                    {data ? (
-                                        <DonutChart attendance={data.asistencias.asistencias} absence={data.asistencias.inasistencias} width={150} height={150} className={styles['donut-chart']} />
-                                    ) : (
-                                        <Card.Text>No tienes asistencias registradas</Card.Text>
-                                    )}
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col md={4} className="mb-4">
-                            <Card className={styles.tarjeta}>
-                                <Card.Body>
-                                    <Card.Title>Materias</Card.Title>
-                                    <Card.Text>No tienes Materias designadas</Card.Text>
-                                    <Button href='/dashboard/materias'>Ver más</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={8} className="mb-4">
-                            <Card className={styles.tarjeta}>
-                                <Card.Body>
-                                    <Card.Title>Cursos</Card.Title>
-                                    {data.horarioProfesor.length > 0 ? (
-                                        data.horarioProfesor.map(curso => (
-                                            <Card.Text key={curso.cursoid}>
-                                                {Object.keys(curso).map(key => (
-                                                    <div key={key}>
-                                                        <strong>{key}:</strong> {curso[key]}
-                                                    </div>
-                                                ))}
-                                            </Card.Text>
-                                        ))
-                                    ) : (
-                                        <Card.Text>No tienes Clases Pendientes.</Card.Text>
-                                    )}
-                                    <Button href='/dashboard/materias'>Ver más</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-
-                        <Col md={4} className="mb-4">
-                            <Card className={styles['tarjeta-QR']}>
-                                <Card.Body>
-                                    <Card.Title>Tu Código QR</Card.Title>
-                                    <QRCodeComponent data={user.token} />
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+            <Container className={styles.tarjetas}>
+                <Row>
+                    <Col md={8} className="mb-4">
+                        <Card className={`${styles.tarjeta} ${styles['tarjeta-asistencias']}`}>
+                            <Card.Body>
+                                <Card.Title>Asistencias</Card.Title>
+                                {data.asistencias ? (
+                                    <DonutChart attendance={data.asistencias.asistencias} absence={data.asistencias.inasistencias} width={150} height={150} className={styles['donut-chart']} />
+                                ) : (
+                                    <Card.Text>No tienes asistencias registradas</Card.Text>
+                                )}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="mb-4">
+                        <Card className={styles.tarjeta}>
+                            <Card.Body>
+                                <Card.Title>Materias</Card.Title>
+                                <Card.Text>No tienes Materias designadas</Card.Text>
+                                <Button href='/dashboard/materias'>Ver más</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={8} className="mb-4">
+                        <Card className={styles.tarjeta}>
+                            <Card.Body>
+                                <Card.Title>Horario de Clases</Card.Title>
+                                {data.horarioProfesor.length > 0 ? (
+                                    data.horarioProfesor.map((curso) => (
+                                        <Card.Text key={curso.cursoid}>
+                                            <strong>Curso:</strong> {curso.cursoid} <br />
+                                            <strong>Día:</strong> {curso.dia} <br />
+                                            <strong>Hora:</strong> {curso.horainicio} - {curso.horafinal}
+                                        </Card.Text>
+                                    ))
+                                ) : (
+                                    <Card.Text>No tienes clases pendientes.</Card.Text>
+                                )}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="mb-4">
+                        <Card className={styles['tarjeta-QR']}>
+                            <Card.Body>
+                                <Card.Title>Tu Código QR</Card.Title>
+                                <QRCodeComponent data={user.token} />
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
