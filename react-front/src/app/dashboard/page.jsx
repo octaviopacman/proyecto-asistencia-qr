6d655dfa599591c25044fe0bd9b9026fcf8bc8cd
AlejoCavuto <165060130+AlejoCavuto@users.noreply.github.com>
2024-11-07 18:50:00 -0300

    Update page.jsx

--- react-front/src/app/dashboard/page.jsx
+++ react-front/src/app/dashboard/page.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useEffect } from 'react';
 import { Nav, Navbar, Container, Card, Col, Row, Button, Dropdown } from 'react-bootstrap';
 import QRCodeComponent from '../login/codigo';
